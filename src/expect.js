e209110cd1ae722eb1d6f1c062f04f03db6d56c6
Olga Tkachenko <pushistaya88@inbox.ru>
2020-07-08 10:31:39 +0700

    hw2-num

--- src/expect.js
+++ src/expect.js
@@ -1,7 +1,64 @@
 export function expect(value) {
   return {
     isNaN: function () {
-      // @todo
+      if (isNaN(value)) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
+    },
+    isPrime: function () {
+      if (typeof value != "number") {
+        console.log(false);
+        return false;
+      }
+      for (let i = 2; i < value; i++) {
+        if (value % i != 0) {
+          console.log(true);
+          return true;
+        } else {
+          console.log(false);
+          return false;
+        }
+      }
+    },
+    isInt: function () {
+      if ((value ^ 0) === value) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
+    },
+    isMultipleOf: function (b) {
+      if (value % b === 0) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
+    },
+    approximately: function (diff) {
+      if (Math.abs(value[0] - value[1]).toFixed(1) == diff) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
+    },
+    isFinite: function () {
+      if (Number.isFinite(value) === true) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
     },
   };
 }
--- src/homeworks/numberAndString/number.js
+++ src/homeworks/numberAndString/number.js
@@ -1,32 +1,42 @@
 import { expect } from "../../expect.js";
 
 // NaN
+console.log("isNan: ");
+
 expect(0).isNaN(); // false
 expect(null).isNaN(); // false
 expect(1).isNaN(); // false;
-expect(NaN).isNaN();
+expect(NaN).isNaN(); // true
 
 // Является ли число простым
+console.log("isPrime: ");
+
 expect(4).isPrime(); // false
 expect(17).isPrime(); // true
 expect("0").isPrime(); // false
 
 // Является ли число целым
+console.log("isInt: ");
+
 expect(10).isInt(); // true
 expect(0.3).isInt(); // false
 expect("0").isInt(); // false
 
 // Является ли число A кратное B
+console.log("isMultipleOf: ");
+
 expect(10).isMultipleOf(5); // true
 expect(9).isMultipleOf(4); // false
 
 // Находятся ли числа близко друг к другу (вне зависимости от знака + или -)
+console.log("approximately: ");
 expect([1, 2]).approximately(0.5); // false. нет. числа не находятся в пределах 0.5
-expect([0.2, 0.1 + 0.2]).approximately(0.1); // false
+expect([0.2, 0.1 + 0.2]).approximately(0.1); // true
 expect([0.2, 0.3]).approximately(0.1); // true
 expect([1, 2]).approximately(1); // true
 
 // Является ли число конечным числом
+console.log("isFinite: ");
 expect(2).isFinite(); // true;
 expect(Infinity).isFinite(); // false
 expect(-Infinity).isFinite(); // false;

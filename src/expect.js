e5351311011f0dac4f0b3af0ceacf2a0ab425eb0
Olga Tkachenko <pushistaya88@inbox.ru>
2020-07-09 21:47:00 +0700

    hw2-str

--- src/expect.js
+++ src/expect.js
@@ -1,7 +1,49 @@
 export function expect(value) {
   return {
-    isNaN: function () {
-      // @todo
+    isTrimmed: function () {
+      if (value == value.trim()) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
+    },
+    isPalindrome: function () {
+      let half = value.slice(0, Math.ceil(value.length / 2));
+      let secondHalf = half.split("").reverse().join("");
+      if (value.includes(secondHalf)) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
+    },
+    within: function (arr) {
+      if (value.startsWith(arr[0]) && value.endsWith(arr[1])) {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
+    },
+    inCase: function (caseName) {
+      let lowerValue = value.toLowerCase();
+      if (value.includes("-") && caseName == "kebab") {
+        console.log(true);
+        return true;
+      } else if (value.includes("_") && caseName == "snake") {
+        console.log(true);
+        return true;
+      } else if (lowerValue != value && caseName == "camel") {
+        console.log(true);
+        return true;
+      } else {
+        console.log(false);
+        return false;
+      }
     },
   };
 }
--- src/homeworks/numberAndString/string.js
+++ src/homeworks/numberAndString/string.js
@@ -1,20 +1,23 @@
 import { expect } from "../../expect.js";
 
 // есть ли пробелы в начале и конце
-
+console.log("isTrimmed: ");
 expect("somebody to love").isTrimmed(); // true
 expect(" somebody to love ").isTrimmed(); // false
 expect("\t\t\tsomebody to love\n").isTrimmed(); //false
 
 // строка читаемая одинаково в обе стороны
+console.log("isPalindrome: ");
 expect("kayak").isPalindrome(); // true
 expect("queen").isPalindrome(); // false
 
 // строка начинается и заканчивается на букву
+console.log("within: ");
 expect("killer queen").within(["k", "n"]); // true
 expect("killer queen").within(["x", "z"]); // false
 
 // Строка оформлена в одном из кейсов (camelCase, snake_case, kebab-case)
+console.log("inCase: ");
 expect("under_pressure").inCase("snake"); // true
 expect("underPressure").inCase("snake"); // false
 
